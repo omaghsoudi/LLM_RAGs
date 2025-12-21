db4ee6f4234250506f683ca91d140ed799969fbf
OmidHM <o.maghsoudi@temple.edu>
2025-12-21 01:28:01 -0500

    Main backup (#1)
    * fixing toml file
    
    * updated toml and got the logger working properly
    
    * rag printing in the logger
    
    ---------
    
    Co-authored-by: omaghsoudi <o.maghsoudi@gmail.com>

---
+++ src/omid_llm/rag_unstrcutured_pdf.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+from omegaconf import DictConfig
+import hydra
+from pathlib import Path
+from omid_llm.modules.initialize import setup_logger
+from omegaconf import OmegaConf
+
+from langchain.chains import RetrievalQA
+from langchain_community.document_loaders import UnstructuredFileLoader
+from langchain_community.llms import Ollama
+from langchain_community.vectorstores import FAISS
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_text_splitters import CharacterTextSplitter
+
+
+@hydra.main(
+    config_path="configs",
+    config_name="rag_unstructured",
+    version_base="1.1.0"
+)
+def run(cfg: DictConfig):
+    logger = setup_logger(path_to_logger="run_hydra.log")
+
+    logger.info(OmegaConf.to_yaml(cfg))
+
+    loader = UnstructuredFileLoader(cfg["unstructured_pdf_file"])
+    docs = loader.load()
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=2000,
+        chunk_overlap=200
+    )
+    texts = text_splitter.split_documents(docs)
+
+    embeddings = HuggingFaceEmbeddings()
+
+    db = FAISS.from_documents(texts, embeddings)
+
+    llm = Ollama(model="llama3")
+
+    chain = RetrievalQA.from_chain_type(
+        llm,
+        retriever=db.as_retriever()
+    )
+
+    question = "Can you please summarize the document"
+    result = chain.invoke({"query": question})
+
+    logger.info(result['result'])
+
+if __name__ == "__main__":
+    run()
